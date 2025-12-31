8508906d75710e26dfe443a24bc6817772d43596
123rabi123 <139615451+123rabi123@users.noreply.github.com>
2025-12-31 19:33:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.getElementById("quiz").style.display = "block";
+function yesLove() {
+  document.getElementById("result").innerHTML =
+    "😍 Mujhe pata tha! Tum meri hi ho ❤️";
+}
+
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  noBtn.style.left = Math.random() * 120 - 60 + "px";
+  noBtn.style.top = Math.random() * 80 - 40 + "px";
+});
